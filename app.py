4b4174dd1b3cc2e613cf24fe71f95bc74273198e
Th@nh Nguy3n <thanhf98@gmail.com>
2016-10-18 22:18:42 +0700

    PetsBook Update 10/18/16

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import *
+from mongoengine import *
+
+connect("c4e6db", host="ds053126.mlab.com", port=53126, username="admin", password="admin")
+
+class Upload_info(Document):
+    img_upload = StringField()
+    des = StringField()
+
+class Pets(Document):
+    name = StringField()
+    img_ava = StringField()
+    rank = IntField()
+    like_count = IntField()
+    upload_info = EmbeddedDocumentField("Upload_info")
+
+class Users(Document):
+    username = StringField()
+    password = StringField()
+    pet = EmbeddedDocumentField("Pets")
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home_page():
+    return render_template("homepage.html", users=Users.objects)
+
+
+if __name__ == '__main__':
+    app.run()
