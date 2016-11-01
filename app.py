3c09342782ee5cc5c86ac76069fb1174195b5946
Th@nh Nguy3n <thanhf98@gmail.com>
2016-11-01 16:57:14 +0700

    Update 1/11/16

--- app.py
+++ app.py
@@ -3,17 +3,18 @@ from mongoengine import *
 
 connect("c4e6db", host="ds053126.mlab.com", port=53126, username="admin", password="admin")
 
-class Upload_info(Document):
-    img_upload = StringField()
-    des = StringField()
+class Upload(Document):
+    img_upload = ListField()
+    caption = ListField()
 
 class Pet(Document):
     name = StringField()
     img_ava = StringField()
+    des = StringField()
     rank = IntField()
     like_count = IntField()
     liked_users = ListField()
-    upload_info = EmbeddedDocumentField("Upload_info")
+    upload = EmbeddedDocumentField("Upload")
 
 class Users(Document):
     username = StringField()
@@ -35,13 +36,13 @@ def home_page():
             if 'signin' in request.form:
                 username_signin = request.form["username_signin"]
                 password_signin = request.form["password_signin"]
-                found_user = False
+                found_user = None
                 for user in Users.objects:
                     if user.username == username_signin and user.password == password_signin:
-                        found_user = True
+                        found_user = user
                         break
                 if found_user:
-                    session["user"] = username_signin
+                    session["user"] = user.username
                     return redirect(url_for("home"))
                 else:
                     alert_signin = "User not found. Please try again!"
@@ -64,8 +65,8 @@ def home():
             liked_user = session["user"]
             rank_list = []
             for user in Users.objects:
-                if user.pet != None:
-                    rank_list.append((user.pet["like_count"], user["username"]))
+                if user.pet is not None:
+                    rank_list.append((user.pet.like_count, user.username))
                     rank_list.sort(reverse=True)
             return render_template("homepageloggedin.html", users=Users.objects, liked_user=liked_user, rank=rank_list)
         elif request.method == "POST":
@@ -80,10 +81,21 @@ def home():
     else:
         return redirect(url_for("home_page"))
 
+@app.route('/profile')
+def get_profile():
+    if "user" not in session:
+        return redirect(url_for("home_page"))
+    username = session["user"]
+    user = Users.objects(username=username).first()
+    if request.method == "GET":
+        return render_template("profile.html", user=user)
+
+
+
 @app.route('/signout')
 def do_signout():
     session.pop("user", None)
     return redirect(url_for("home_page"))
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5005)
